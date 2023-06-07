07ec01b5cd44763373eb60ebbae7bea41ecf3d64
Kim Hyeon woo <eric-kim@naver.com>
2023-06-07 21:10:57 +0900

    업,다운캐스팅 연습. 오버라이딩해서 같은 메소드여도 다른 결과를 얻을 수 있다. 반복문이 훨씬 간결해짐.

---
+++ src/Upcasting.java
@@ -0,0 +1,44 @@
+public class Upcasting {
+    public static void main(String[] args) {
+
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+        Animal goat = new Goat();
+
+        Animal[] zoo = {dog, cat, goat};
+
+        for(Animal mal : zoo){
+            mal.bark(); //같은 메소드이지만 각 오버라이딩된 메소드로 실행된다.
+        }
+    }
+}
+
+class Animal {
+    String feature;
+    int size;
+
+    void bark(){
+        System.out.println("짖다!");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void bark(){
+        System.out.println("왈왈!");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void bark(){
+        System.out.println("이야오옹~");
+    }
+}
+
+class Goat extends Animal {
+    @Override
+    void bark(){
+        System.out.println("곹.곹.");
+    }
+}
--- src/stringPrac.java
+++ src/stringPrac.java
@@ -1,7 +1,14 @@
+import java.util.Locale;
+
 public class stringPrac {
     public static void main(String[] args) {
         String str = "string";
-        String printStr = str.toUpperCase();
+        String printStr = str.toLowerCase();
+        printStr.toUpperCase();
+
+
+
+
 
         System.out.println(printStr);
     }
