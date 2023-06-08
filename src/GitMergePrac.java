19a6e678a42f20bc5974c8f498d020ed2d1bc0b6
hyeonwookim <wooin.dev@gmail.com>
2023-06-08 21:04:00 +0900

    3에서 수정완료!!

--- src/GitMergePrac.java
+++ src/GitMergePrac.java
@@ -2,6 +2,6 @@ public class GitMergePrac {
     public static void main(String[] args) {
         String str1 = "마스터 브랜치";
         String str2 = "2에서 수정해주세요.";
-        String str3 = "3에서 수정해주세요.";
+        String str3 = "3에서 수정완료!!!!!!!";
     }
 }
